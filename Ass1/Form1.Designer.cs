51429a3cf44adaacf1c1cb09aab3a1880f687fdd
912-Eftimie-Eduard <eduard.eftimie@stud.ubbcluj.ro>
2024-10-16 22:46:17 +0300

    Add project files.

---
+++ Ass1/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace Ass1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.playerGrid = new System.Windows.Forms.DataGridView();
+            this.playersBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.homeworkDataSet = new Ass1.HomeworkDataSet();
+            this.playersTableAdapter = new Ass1.HomeworkDataSetTableAdapters.PlayersTableAdapter();
+            this.fillByToolStrip = new System.Windows.Forms.ToolStrip();
+            this.fillByToolStripButton = new System.Windows.Forms.ToolStripButton();
+            this.teamGrid = new System.Windows.Forms.DataGridView();
+            this.teamsBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.homeworkDataSet1 = new Ass1.HomeworkDataSet1();
+            this.teamsTableAdapter = new Ass1.HomeworkDataSet1TableAdapters.TeamsTableAdapter();
+            this.editButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.playerGrid)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.playersBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.homeworkDataSet)).BeginInit();
+            this.fillByToolStrip.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.teamGrid)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.teamsBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.homeworkDataSet1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // playerGrid
+            // 
+            this.playerGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.playerGrid.Location = new System.Drawing.Point(473, 60);
+            this.playerGrid.Margin = new System.Windows.Forms.Padding(4);
+            this.playerGrid.Name = "playerGrid";
+            this.playerGrid.RowHeadersWidth = 51;
+            this.playerGrid.Size = new System.Drawing.Size(371, 185);
+            this.playerGrid.TabIndex = 0;
+            // 
+            // playersBindingSource
+            // 
+            this.playersBindingSource.DataMember = "Players";
+            this.playersBindingSource.DataSource = this.homeworkDataSet;
+            // 
+            // homeworkDataSet
+            // 
+            this.homeworkDataSet.DataSetName = "HomeworkDataSet";
+            this.homeworkDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // playersTableAdapter
+            // 
+            this.playersTableAdapter.ClearBeforeFill = true;
+            // 
+            // fillByToolStrip
+            // 
+            this.fillByToolStrip.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.fillByToolStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fillByToolStripButton});
+            this.fillByToolStrip.Location = new System.Drawing.Point(0, 0);
+            this.fillByToolStrip.Name = "fillByToolStrip";
+            this.fillByToolStrip.Size = new System.Drawing.Size(905, 27);
+            this.fillByToolStrip.TabIndex = 1;
+            this.fillByToolStrip.Text = "fillByToolStrip";
+            // 
+            // fillByToolStripButton
+            // 
+            this.fillByToolStripButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.fillByToolStripButton.Name = "fillByToolStripButton";
+            this.fillByToolStripButton.Size = new System.Drawing.Size(48, 24);
+            this.fillByToolStripButton.Text = "FillBy";
+            this.fillByToolStripButton.Click += new System.EventHandler(this.fillByToolStripButton_Click);
+            // 
+            // teamGrid
+            // 
+            this.teamGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.teamGrid.Location = new System.Drawing.Point(53, 60);
+            this.teamGrid.Name = "teamGrid";
+            this.teamGrid.RowHeadersWidth = 51;
+            this.teamGrid.RowTemplate.Height = 24;
+            this.teamGrid.Size = new System.Drawing.Size(371, 185);
+            this.teamGrid.TabIndex = 2;
+            this.teamGrid.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.teamGrid_CellContentClick);
+            // 
+            // teamsBindingSource
+            // 
+            this.teamsBindingSource.DataMember = "Teams";
+            this.teamsBindingSource.DataSource = this.homeworkDataSet1;
+            // 
+            // homeworkDataSet1
+            // 
+            this.homeworkDataSet1.DataSetName = "HomeworkDataSet1";
+            this.homeworkDataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // teamsTableAdapter
+            // 
+            this.teamsTableAdapter.ClearBeforeFill = true;
+            // 
+            // editButton
+            // 
+            this.editButton.Location = new System.Drawing.Point(413, 289);
+            this.editButton.Name = "editButton";
+            this.editButton.Size = new System.Drawing.Size(75, 23);
+            this.editButton.TabIndex = 3;
+            this.editButton.Text = "Edit";
+            this.editButton.UseVisualStyleBackColor = true;
+            this.editButton.Click += new System.EventHandler(this.editButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(905, 554);
+            this.Controls.Add(this.editButton);
+            this.Controls.Add(this.teamGrid);
+            this.Controls.Add(this.fillByToolStrip);
+            this.Controls.Add(this.playerGrid);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "w";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.playerGrid)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.playersBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.homeworkDataSet)).EndInit();
+            this.fillByToolStrip.ResumeLayout(false);
+            this.fillByToolStrip.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.teamGrid)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.teamsBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.homeworkDataSet1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView playerGrid;
+        private HomeworkDataSet homeworkDataSet;
+        private System.Windows.Forms.BindingSource playersBindingSource;
+        private HomeworkDataSetTableAdapters.PlayersTableAdapter playersTableAdapter;
+        private System.Windows.Forms.ToolStrip fillByToolStrip;
+        private System.Windows.Forms.ToolStripButton fillByToolStripButton;
+        private System.Windows.Forms.DataGridView teamGrid;
+        private HomeworkDataSet1 homeworkDataSet1;
+        private System.Windows.Forms.BindingSource teamsBindingSource;
+        private HomeworkDataSet1TableAdapters.TeamsTableAdapter teamsTableAdapter;
+        private System.Windows.Forms.Button editButton;
+    }
+}
+
---
+++ Ass1/Form1.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+
+namespace Ass1
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection connection;
+        SqlDataAdapter DataAdapterParent;
+        SqlDataAdapter DataAdapterChild;
+        SqlCommandBuilder cmdBuilder;
+        DataSet dset;
+
+        string FormCaption;
+        string ConnectionString;
+        string ChildQuery;
+        string ChildString;
+        string ChildFillQuery;
+
+        string ParentQuery;
+        string ParentString;
+        string ParentFillQuery;
+
+        string OTMString;
+        string Key;
+
+        void FillData()
+        {
+            try
+            {
+                this.connection = new SqlConnection(this.ConnectionString);
+
+                ParentFillQuery = this.ParentQuery;
+                ChildFillQuery = this.ChildQuery;
+
+                DataAdapterParent = new SqlDataAdapter(ParentFillQuery, connection);
+                DataAdapterChild = new SqlDataAdapter(ChildFillQuery, connection);
+                dset = new DataSet();
+
+                // Log information
+                Console.WriteLine("Executing Parent query: " + ParentFillQuery);
+                Console.WriteLine("Executing Child query: " + ChildFillQuery);
+
+                DataAdapterParent.Fill(dset, this.ParentString);
+                DataAdapterChild.Fill(dset, this.ChildString);
+
+                // Log data count
+                Console.WriteLine("Parent data count: " + dset.Tables[this.ParentString].Rows.Count);
+
+                // Check if ChildString table exists in the dataset
+                if (dset.Tables.Contains(this.ChildString))
+                {
+                    Console.WriteLine("Child data count: " + dset.Tables[this.ChildString].Rows.Count);
+
+                    cmdBuilder = new SqlCommandBuilder(DataAdapterChild);
+
+                    dset.Relations.Add(this.OTMString,
+                        dset.Tables[this.ParentString].Columns[Key],
+                        dset.Tables[this.ChildString].Columns[Key]);
+                    this.teamGrid.DataSource = dset.Tables[this.ParentString];
+                    this.playerGrid.DataSource = this.teamGrid.DataSource;
+                    this.playerGrid.DataMember = this.OTMString;
+                }
+                else
+                {
+                    MessageBox.Show("Child table '" + this.ChildString + "' does not exist in the dataset.");
+                }
+            }
+            catch (InvalidOperationException ex)
+            {
+                MessageBox.Show("Invalid operation: " + ex.Message);
+                Console.WriteLine(ex.StackTrace); // Print the stack trace for detailed information
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error during data filling: " + ex.Message);
+                Console.WriteLine(ex.StackTrace); // Print the stack trace for detailed information
+            }
+        }
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.FormCaption = ConfigurationManager.AppSettings["FormCation"];
+            this.Text = FormCaption;
+            this.ConnectionString = ConfigurationManager.AppSettings["ConnectionString"];
+
+            this.ParentQuery = ConfigurationManager.AppSettings["ParentQuery"];
+            this.ChildQuery = ConfigurationManager.AppSettings["ChildQuery"];
+
+            this.ParentString = ConfigurationManager.AppSettings["ParentString"];
+            this.ChildString = ConfigurationManager.AppSettings["ChildString"];
+            this.OTMString = ConfigurationManager.AppSettings["OTMString"];
+
+            this.Key = ConfigurationManager.AppSettings["Key"];
+
+            FillData();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.FormCaption = ConfigurationManager.AppSettings["FormCation"];
+            this.Text = FormCaption;
+            this.ConnectionString = ConfigurationManager.AppSettings["ConnectionString"];
+
+            this.ParentQuery = ConfigurationManager.AppSettings["ParentQuery"];
+            this.ChildQuery = ConfigurationManager.AppSettings["ChildQuery"];
+
+            this.ParentString = ConfigurationManager.AppSettings["ParentString"];
+            this.ChildString = ConfigurationManager.AppSettings["ChildString"];
+            this.OTMString = ConfigurationManager.AppSettings["OTMString"];
+
+            this.Key = ConfigurationManager.AppSettings["Key"];
+
+            //string sqlCommand = "SELECT * FROM Teams";
+
+            //SqlCommand getTeams = new SqlCommand(sqlCommand, connection);
+            //DataAdapterChild = new SqlDataAdapter(getTeams);
+            //DataAdapterChild.Fill(dset, "Teams");
+            //teamGrid.DataSource = dset.Tables["Teams"];
+            //MessageBox.Show(ConfigurationManager.AppSettings["greeting"]);
+
+        }
+
+        private void fillByToolStripButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                this.playersTableAdapter.FillBy(this.homeworkDataSet.Players);
+            }
+            catch (System.Exception ex)
+            {
+                System.Windows.Forms.MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        //private void playerGrid_CellClick(object sender, DataGridViewCellEventArgs e)
+        //{
+        //    if (e.RowIndex >= 0 && e.RowIndex < teamGrid.Rows.Count - 1)
+        //    {
+        //        var id = teamGrid.Rows[e.RowIndex].Cells[0].Value;
+
+        //        string sqlCommand = "SELECT * FROM Players WHERE team_id = " + id;
+
+        //        SqlCommand getPlayersByTeam = new SqlCommand(sqlCommand, connection);
+        //        getPlayersByTeam.Parameters.AddWithValue("@team_id", id);
+        //
+        //       dset = new DataSet();
+        //        DataAdapterParent = new SqlDataAdapter(getPlayersByTeam);
+        //        DataAdapterParent.Fill(dset, "Players");
+        //        playerGrid.DataSource = dset.Tables["Players"];
+        //    }
+        //}
+
+        private void teamGrid_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+        //    playerGrid_CellClick(sender, e);
+        }
+
+        private void editButton_Click(object sender, EventArgs e)
+        {
+           try{
+                playerGrid.EndEdit();
+                if (dset.HasChanges()){
+                    SqlCommandBuilder builerChild = new SqlCommandBuilder(DataAdapterChild);
+                    SqlCommandBuilder builder = new SqlCommandBuilder(DataAdapterParent);
+                    //dat.Update(ds, "Teams");
+                    //foreach (DataRow row in dset.Tables["Players"].Rows){
+
+                    //    if (row.RowState == DataRowState.Deleted) {
+                            //in the DataRowCollection
+                     //       var playerId = row["player_id", DataRowVersion.Original];
+                      //      DeleteRelatedPositions(playerId);
+                     //       row.Delete();
+                     //   }
+                    ///}
+                    DataAdapterParent.Update(dset, this.ParentString);
+                    DataAdapterChild.Update(dset, this.ChildString);
+                    MessageBox.Show("Changes saved");
+                } else {
+                    MessageBox.Show("No changes to save");
+                }
+            } catch (Exception ex){
+                MessageBox.Show("Error on edit: " + ex.Message);
+            }
+        }
+
+        //private void DeleteRelatedPositions(object playerId){
+        //    //MessageBox.Show("aici");
+        //    using (SqlConnection connection = new SqlConnection("Data Source=DESKTOP-LQ1SO6G\\SQLEXPRESS;Initial Catalog=Homework;Integrated Security=True")){
+        //        connection.Open();
+        //        string sqlCommand = "DELETE FROM MergedPlayersPositions WHERE player_id = @player_id";
+        //        using (SqlCommand deletePositions = new SqlCommand(sqlCommand, connection)){
+        //            deletePositions.Parameters.AddWithValue("@player_id", playerId);
+        //            deletePositions.ExecuteNonQuery();
+        //        }
+        //    }
+        //} 
+    }
+}
